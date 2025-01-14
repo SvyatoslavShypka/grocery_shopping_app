271c4fe0cff3bafaf9575ce22506b28558c77f41
Svyatoslav Shypka <svyat200909@gmail.com>
2025-01-14 19:11:40 +0100

    home_screen start

---
+++ lib/navigator/app_router.dart
@@ -0,0 +1,101 @@
+import 'package:flutter/material.dart';
+// import 'package:flutter_animate/flutter_animate.dart';
+import 'package:go_router/go_router.dart';
+
+import '../screens/intro_screen.dart';
+
+class AppRouter {
+  late final GoRouter router = GoRouter(
+    routes: <RouteBase>[
+      GoRoute(
+        name: 'intro',
+        path: '/',
+        builder: (BuildContext context, GoRouterState state) {
+          return const IntroScreen();
+        },
+      ),
+      // GoRoute(
+      // name: 'login',
+      // path: '/login',
+      // builder: (BuildContext context, GoRouterState state) {
+      // return const LoginScreen();
+      // },
+      // ),
+      // GoRoute(
+      // name: 'register',
+      // path: '/register',
+      // builder: (BuildContext context, GoRouterState state) {
+      // return const RegisterScreen();
+      // },
+      // ),
+      // GoRoute(
+      // name: 'home',
+      // path: '/home',
+      // builder: (BuildContext context, GoRouterState state) {
+      // return const HomeScreen();
+      // },
+      // ),
+      // GoRoute(
+      // name: 'scan',
+      // path: '/scan',
+      // builder: (BuildContext context, GoRouterState state) {
+      // return const ScanScreen();
+      // },
+      // ),
+      // GoRoute(
+      // name: 'orders',
+      // path: '/orders',
+      // builder: (BuildContext context, GoRouterState state) {
+      // return const OrderScreen();
+      // },
+      // routes: [
+      // GoRoute(
+      // name: 'order-details',
+      // path: 'orders/:orderId',
+      // builder: (BuildContext context, GoRouterState state) {
+      // return const OrderDetailsScreen();
+      // },
+      // ),
+      // ]),
+      // GoRoute(
+      // name: 'cart',
+      // path: '/cart',
+      // builder: (BuildContext context, GoRouterState state) {
+      // return const CartScreen();
+      // },
+      // ),
+      // GoRoute(
+      // name: 'category',
+      // path: '/category/:categoryId',
+      // builder: (BuildContext context, GoRouterState state) {
+      // return CategoryScreen(
+      // categoryId: state.pathParameters['categoryId']!,
+      // );
+      // },
+      // ),
+      // GoRoute(
+      // name: 'search',
+      // path: '/search',
+      // pageBuilder: (BuildContext context, GoRouterState state) {
+      // return CustomTransitionPage<void>(
+      // key: state.pageKey,
+      // transitionDuration: const Duration(milliseconds: 400),
+      // reverseTransitionDuration: const Duration(milliseconds: 400),
+      // transitionsBuilder: (
+      // BuildContext context,
+      // Animation<double> animation,
+      // Animation<double> secondaryAnimation,
+      // Widget child,
+      // ) {
+      // return child.animate().fadeIn(
+      // duration: const Duration(milliseconds: 400),
+      // begin: 0.0,
+      // );
+      // },
+      // child: const SearchScreen(),
+      // );
+      // },
+      // ),
+    ],
+  );
+}
---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final textTheme = Theme.of(context).textTheme;
+    final colorScheme = Theme.of(context).colorScheme;
+
+    return Scaffold(
+      appBar:
+          AppBar(backgroundColor: colorScheme.primaryContainer, actions: []),
+    );
+  }
+}
