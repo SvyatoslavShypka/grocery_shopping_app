343c731560bb9e8b5d26d5c013c91adfd9e9a5e6
Svyatoslav Shypka <svyat200909@gmail.com>
2025-01-22 18:33:00 +0100

    sc7

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -34,7 +34,7 @@ class HomeScreen extends StatelessWidget {
                 ),
                 const SizedBox(width: 8.0),
                 const Icon(Icons.arrow_drop_down),
-              ], //check again one more time 6
+              ],
             )
           ],
         ),
@@ -48,6 +48,35 @@ class HomeScreen extends StatelessWidget {
             ),
           ),
         ],
+        bottom: PreferredSize(
+          preferredSize: Size.fromHeight(56.0),
+          child: Padding(
+            padding: const EdgeInsets.only(
+              left: 16.0,
+              right: 16.0,
+              bottom: 8.0,
+            ),
+            child: TextFormField(
+              decoration: InputDecoration(
+                hintText: 'Search',
+                hintStyle: textTheme.bodyMedium!.copyWith(
+                  color: colorScheme.onPrimary.withAlpha(200),
+                ),
+                prefixIcon: Icon(
+                  Icons.search,
+                  color: colorScheme.onPrimary,
+                ),
+                contentPadding: const EdgeInsets.symmetric(horizontal: 16.0),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(32.0),
+                  borderSide: BorderSide.none,
+                ),
+                filled: true,
+                fillColor: colorScheme.onPrimaryContainer.withAlpha(100), //sc7
+              ),
+            ),
+          ),
+        ),
       ),
     );
   }
