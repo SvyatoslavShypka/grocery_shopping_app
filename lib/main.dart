af8f5905483af31877620bbaed394d582ff93795
Svyatoslav Shypka <svyat200909@gmail.com>
2025-02-20 07:20:21 +0100

    sc24-1

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'package:grocery_shopping_app/navigator/app_router.dart';
 
 void main() {
   runApp(const MyApp());
-} //sc23
+} //sc24-1
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
