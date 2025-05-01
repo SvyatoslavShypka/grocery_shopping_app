70b2feda6a98e800be919d8e104631bbb6ff857d
Svyatoslav Shypka <svyat200909@gmail.com>
2025-05-01 14:10:29 +0200

    40:59 get methods

---
+++ api/routes/categories/[categoryId]/index.dart
@@ -0,0 +1,30 @@
+import 'dart:async';
+import 'dart:io';
+
+// import 'package:category_repository/category_repository.dart';
+import 'package:dart_frog/dart_frog.dart';
+
+FutureOr<Response> onRequest(
+  RequestContext context,
+  String categoryId,
+) async {
+  switch (context.request.method) {
+    case HttpMethod.get:
+      return _get(context, categoryId);
+    case HttpMethod.put:
+    case HttpMethod.delete:
+    case HttpMethod.head:
+    case HttpMethod.options:
+    case HttpMethod.patch:
+    case HttpMethod.post:
+      return Response(statusCode: HttpStatus.methodNotAllowed);
+  }
+}
+
+Future<Response> _get(RequestContext context, String categoryId) async {
+  // Return one categoriy
+  // final categoryRepository = context.read<CategoryRepository>();
+  // final category = await categoryRepository.getCategoryById(categoryId);
+  // return Response.json(body: category);
+  return Response.json(body: []);
+}
---
+++ api/routes/categories/[categoryId]/products/index.dart
@@ -0,0 +1,30 @@
+import 'dart:async';
+import 'dart:io';
+
+import 'package:dart_frog/dart_frog.dart';
+// import 'package:product_repository/product_repository.dart';
+
+FutureOr<Response> onRequest(
+  RequestContext context,
+  String categoryId,
+) async {
+  switch (context.request.method) {
+    case HttpMethod.get:
+      return _get(context, categoryId);
+    case HttpMethod.put:
+    case HttpMethod.delete:
+    case HttpMethod.head:
+    case HttpMethod.options:
+    case HttpMethod.patch:
+    case HttpMethod.post:
+      return Response(statusCode: HttpStatus.methodNotAllowed);
+  }
+}
+
+Future<Response> _get(RequestContext context, String categoryId) async {
+  // Return all the products for the selected category
+  // final productRepository = context.read<ProductRepository>();
+  // final products = await productRepository.getProductsByCategoryId(categoryId);
+  // return Response.json(body: products);
+  return Response.json(body: []);
+}
