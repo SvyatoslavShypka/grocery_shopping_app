1d76563b6440af52b1b6548c305251e56a009701
Svyatoslav Shypka <svyat200909@gmail.com>
2025-05-01 11:42:28 +0200

    33:48 top left radius near Home button

--- lib/widgets/app_bottom_nav_bar.dart
+++ lib/widgets/app_bottom_nav_bar.dart
@@ -7,52 +7,72 @@ class AppBottomNavBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-        child: NavigationBar(
-            backgroundColor: Theme.of(context).colorScheme.secondary,
-            height: 60,
-            selectedIndex: index,
-            indicatorColor: Colors.transparent,
-            destinations: [
-          Padding(
-            padding: EdgeInsets.only(top: 8.0),
-            child: NavigationDestination(
-              selectedIcon: Icon(Icons.home, color: Colors.white),
-              icon: Icon(Icons.home_outlined, color: Colors.white),
-              label: 'Home',
+        clipBehavior: Clip.antiAlias,
+        decoration: const BoxDecoration(
+          borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(48.0),
+          ),
+        ),
+        child: NavigationBarTheme(
+          data: NavigationBarThemeData(
+            labelTextStyle: MaterialStatePropertyAll(
+              Theme.of(context).textTheme.bodySmall!.copyWith(
+                    color: Colors.white,
+                  ),
             ),
           ),
-          Padding(
-            padding: EdgeInsets.only(top: 8.0),
-            child: NavigationDestination(
-              selectedIcon: Icon(Icons.shopping_bag, color: Colors.white),
-              icon: Icon(Icons.shopping_bag_outlined, color: Colors.white),
-              label: 'Browse',
-            ),
-          ),
-          Padding(
-            padding: EdgeInsets.only(top: 8.0),
-            child: NavigationDestination(
-              selectedIcon: Icon(Icons.qr_code, color: Colors.white),
-              icon: Icon(Icons.qr_code_2_outlined, color: Colors.white),
-              label: 'Scan',
-            ),
-          ),
-          Padding(
-            padding: EdgeInsets.only(top: 8.0),
-            child: NavigationDestination(
-              selectedIcon: Icon(Icons.shopping_bag, color: Colors.white),
-              icon: Icon(Icons.shopping_bag_outlined, color: Colors.white),
-              label: 'Orders',
-            ),
-          ),
-          Padding(
-            padding: EdgeInsets.only(top: 8.0),
-            child: NavigationDestination(
-              selectedIcon: Icon(Icons.qr_code, color: Colors.white),
-              icon: Icon(Icons.qr_code_2_outlined, color: Colors.white),
-              label: 'Profile',
-            ),
-          ),
-        ]));
+          child: NavigationBar(
+              backgroundColor: Theme.of(context).colorScheme.secondary,
+              height: 60,
+              selectedIndex: index,
+              indicatorColor: Colors.transparent,
+              onDestinationSelected: (value) {
+                print(value);
+              },
+              destinations: const [
+                Padding(
+                  padding: EdgeInsets.only(top: 8.0),
+                  child: NavigationDestination(
+                    selectedIcon: Icon(Icons.home, color: Colors.white),
+                    icon: Icon(Icons.home_outlined, color: Colors.white),
+                    label: 'Home',
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(top: 8.0),
+                  child: NavigationDestination(
+                    selectedIcon: Icon(Icons.shopping_bag, color: Colors.white),
+                    icon:
+                        Icon(Icons.shopping_bag_outlined, color: Colors.white),
+                    label: 'Browse',
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(top: 8.0),
+                  child: NavigationDestination(
+                    selectedIcon: Icon(Icons.qr_code, color: Colors.white),
+                    icon: Icon(Icons.qr_code_2_outlined, color: Colors.white),
+                    label: 'Scan',
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(top: 8.0),
+                  child: NavigationDestination(
+                    selectedIcon: Icon(Icons.shopping_bag, color: Colors.white),
+                    icon:
+                        Icon(Icons.shopping_bag_outlined, color: Colors.white),
+                    label: 'Orders',
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(top: 8.0),
+                  child: NavigationDestination(
+                    selectedIcon: Icon(Icons.qr_code, color: Colors.white),
+                    icon: Icon(Icons.qr_code_2_outlined, color: Colors.white),
+                    label: 'Profile',
+                  ),
+                ),
+              ]),
+        ));
   }
 }
