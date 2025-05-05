838fafa6fbd51f557709aedfd23d5e68ac379d0f
Svyatoslav Shypka <svyat200909@gmail.com>
2025-05-05 16:29:45 +0200

    sc_002

--- api/packages/models/lib/src/models/category.dart
+++ api/packages/models/lib/src/models/category.dart
@@ -1,10 +1,4 @@
 import 'package:equatable/equatable.dart';
-// import 'package:grocery_shopping_app/env.dart';
-import '../../../../../../../lib/env.dart';
-
-String apiKey = Env.apiKey;
-String baseUrl = Env.baseUrl;
-String passwordKey = Env.passwordKey;
 
 enum CategorySortBy {
   lowToHighPrice(
